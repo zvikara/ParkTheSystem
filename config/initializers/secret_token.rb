7f2c5064e2105871c9d519bc248ca406a290e8c2
Zvika Rap <zvikara@gmail.com>
2013-05-25 09:00:34 +0300

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails3ParkTheSystem::Application.config.secret_key_base = '70225d115556e565ae71d52cc205cb5cc96a34368e49382fce586b90b2cb7ac93e0d1bdea512ff38b2341b3f5f4097453bc530fcfd4a9ef0b78fcdf423fc1afe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails3ParkTheSystem::Application.config.session_store :cookie_store, key: '_rails3-park-the-system_session'
